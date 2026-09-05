214a26b737c2c5513d1e41898fa93333077543f2
AvalonWot <skeu.grass@gmail.com>
2026-09-05 09:47:47 +0800

    修复骷髅和神兽死亡时RefreshAll中没有判断master被设置为空的bug

--- Server/MirObjects/Monsters/BoneFamiliar.cs
+++ Server/MirObjects/Monsters/BoneFamiliar.cs
@@ -15,10 +15,13 @@ namespace Server.MirObjects.Monsters
         public override void RefreshAll()
         {
             base.RefreshAll();
-            Stats[Stat.MinDC] += (int)Math.Floor(master.Stats[Stat.MinSC] * (PetLevel * PetLevel * 0.005f));
-            Stats[Stat.MaxDC] += (int)Math.Floor(master.Stats[Stat.MaxSC] * (PetLevel * PetLevel * 0.005f));
-            Stats[Stat.Accuracy] += (int)Math.Floor((master.Stats[Stat.MinSC] + master.Stats[Stat.MaxSC]) / 5.0f);
-            Stats[Stat.Agility] += (int)Math.Floor((master.Stats[Stat.MinSC] + master.Stats[Stat.MaxSC]) / 10.0f);
+            if (master != null)
+            {
+                Stats[Stat.MinDC] += (int)Math.Floor(master.Stats[Stat.MinSC] * (PetLevel * PetLevel * 0.005f));
+                Stats[Stat.MaxDC] += (int)Math.Floor(master.Stats[Stat.MaxSC] * (PetLevel * PetLevel * 0.005f));
+                Stats[Stat.Accuracy] += (int)Math.Floor((master.Stats[Stat.MinSC] + master.Stats[Stat.MaxSC]) / 5.0f);
+                Stats[Stat.Agility] += (int)Math.Floor((master.Stats[Stat.MinSC] + master.Stats[Stat.MaxSC]) / 10.0f);
+            }
         }
         
         public override void Spawned()
